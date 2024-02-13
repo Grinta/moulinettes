e22178b43e8582a513abe83606eab1b76822db47
Thomas <soler.thomas.06@gmail.com>
2024-02-13 12:27:34 +0100

    Add options to open file in text editor
    Adds 2 options:
      - `editor` (string) allows specifying an editor. Default: system
        editor.
      - `editor` (boolean) determines if file should be open or not on
        creation. Default: true.

--- lib/generators/moulinettes/moulinette_generator.rb
+++ lib/generators/moulinettes/moulinette_generator.rb
@@ -3,6 +3,9 @@ require "rails/generators"
 module Moulinettes
   module Generators
     class MoulinetteGenerator < Rails::Generators::NamedBase
+      class_option :editor, type: :string, default: $EDITOR, desc: "Open the generated file in the specified editor"
+      class_option :open, type: :boolean, default: true, desc: "Open the generated file in your editor"
+      
       desc "This generator creates a task file following the moulinettes template"
 
       source_root File.expand_path(File.dirname(__FILE__))
@@ -11,6 +14,10 @@ module Moulinettes
         copy_file "yyyymmdd_action_of_a_moulinette_task.rake", filepath
         gsub_file filepath, "action_of_a_moulinette_task", task_name
         gsub_file filepath, "A description of the task goes here", task_description
+
+        if options["open"]
+          system("#{options["editor"]} #{filepath}")
+        end
       end
 
       def timestamp
